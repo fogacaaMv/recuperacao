4e00177a3990523d573156d583f8d57c872addb2
fogacaaMv <150248530+fogacaaMv@users.noreply.github.com>
2023-12-06 09:13:58 -0300

    Add files via upload

--- divisor.js
+++ divisor.js
@@ -2,11 +2,16 @@ const frm = document.querySelector("form")
 const resp =document.querySelector("h3")
 
 //ouvinte
-frm.addEventListener("submit"), (e) => {
-    e.perventefault()
+frm.addEventListener("submit", (e) => {
+    e.preventDefault()
 
-    const num = nimber(frm. inNumero.vale)
+    const num = Number(frm. inNumero.value)
     let resposta ="  " // não precisa verificar se é divisivel por 1
 
-    for(let i =; i <=num/2; i++){   // não precisa mais que a meta do numero.
+    for(let i =1; i <=num/2; i++){   // não precisa mais que a meta do numero.
     if(num% i==0){
+        resposta = resposta + i + " ,"
+       }
+    }
+    resp.innerText=`divisor de ${num}: ${resposta}${num}.`
+})
